281a4dbe018e3e20a1ab321fefc71fbf5862e447
nbloomf <nathan.bloomfield@automattic.com>
2020-07-13 15:22:58 -0500

    Use multiPartnerCardProcessor in composite checkout

--- client/my-sites/checkout/composite-checkout/composite-checkout.js
+++ client/my-sites/checkout/composite-checkout/composite-checkout.js
@@ -53,7 +53,7 @@ import useCreatePaymentMethods from './use-create-payment-methods';
 import {
 	applePayProcessor,
 	freePurchaseProcessor,
-	stripeCardProcessor,
+	multiPartnerCardProcessor,
 	fullCreditsProcessor,
 	existingCardProcessor,
 	payPalProcessor,
@@ -493,7 +493,7 @@ export default function CompositeCheckout( {
 		() => ( {
 			'apple-pay': applePayProcessor,
 			'free-purchase': freePurchaseProcessor,
-			card: stripeCardProcessor,
+			card: multiPartnerCardProcessor,
 			alipay: ( transactionData ) =>
 				genericRedirectProcessor( 'alipay', transactionData, getThankYouUrl, siteSlug ),
 			p24: ( transactionData ) =>
