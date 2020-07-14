512e40245cd27e7a4b071f1fff9c954f6b499bc7
nsakaimbo <nsakaimbo@me.com>
2020-07-14 14:26:28 -0400

    temp: dont notarize

--- desktop/bin/after_sign_hook.js
+++ desktop/bin/after_sign_hook.js
@@ -9,8 +9,9 @@ const NOTARIZATION_PWD = process.env.WPDESKTOP_NOTARIZATION_PWD;
 const NOTARIZATION_ASC_PROVIDER = process.env.APPLE_DEVELOPER_SHORT_NAME;
 
 const circleTag = process.env.CIRCLE_TAG;
-const shouldNotarize =
-	process.platform === 'darwin' && !! circleTag && circleTag.startsWith( 'desktop' );
+// const shouldNotarize =
+	// process.platform === 'darwin' && !! circleTag && circleTag.startsWith( 'desktop' );
+const shouldNotarize = false;
 
 function elapsed( start ) {
 	const now = new Date();
---
+++ desktop/bin/update-yaml.js
@@ -0,0 +1,1 @@
+// https://gist.github.com/nsakaimbo/7fda1414bf0d40796277f40a2592dad5
