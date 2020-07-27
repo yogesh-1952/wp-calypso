33f73431328ff3cbe307b5060f3403d089adca6f
Payton Swick <payton@foolord.com>
2020-07-27 13:04:05 -0400

    Only send either email or alternateEmail to G Suite validation

--- client/my-sites/checkout/composite-checkout/wpcom/types/wpcom-store-state.ts
+++ client/my-sites/checkout/composite-checkout/wpcom/types/wpcom-store-state.ts
@@ -600,8 +600,9 @@ export function prepareGSuiteContactValidationRequest(
 		contact_information: {
 			firstName: details.firstName?.value ?? '',
 			lastName: details.lastName?.value ?? '',
-			...( details.email?.value && { email: details.email?.value } ),
-			...( details.alternateEmail?.value && { email: details.alternateEmail?.value } ),
+			...( details.alternateEmail?.value && { alternateEmail: details.alternateEmail?.value } ),
+			...( ! details.alternateEmail?.value &&
+				details.email?.value && { email: details.email?.value } ),
 			postalCode: details.postalCode?.value ?? '',
 			countryCode: details.countryCode?.value ?? '',
 		},
