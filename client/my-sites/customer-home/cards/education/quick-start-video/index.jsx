a99f8caa2bc25db3a0ea9a96216c0294bc25a061
Don Lair <don.lair@gmail.com>
2020-07-27 10:58:25 -0400

    Updating the card to point to the correct support page and to use the correct screenshot.

--- client/my-sites/customer-home/cards/education/quick-start-video/index.jsx
+++ client/my-sites/customer-home/cards/education/quick-start-video/index.jsx
@@ -30,8 +30,8 @@ export const QuickStartVideo = () => {
 			<Card>
 				<div>
 					<InlineSupportLink
-						supportPostId={ 4454 }
-						supportLink={ localizeUrl( 'https://wordpress.com/support/stats/' ) }
+						supportPostId={ 81083 }
+						supportLink={ localizeUrl( 'https://wordpress.com/support/start/' ) }
 						showIcon={ false }
 						showText={ false }
 						showHero={ true }
